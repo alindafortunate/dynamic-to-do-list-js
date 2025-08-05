bfc16e730793a2eb9ca05a72947b857e8c5645ce
alindafortunate <alindafortunate5@gmail.com>
2025-08-05 16:10:54 +0300

    Understood the shortcut of looping throught the array.

---
+++ practice.js
@@ -0,0 +1,14 @@
+// Working with arrays.
+
+let fruits = ['mango', 'banana', 'apple']
+
+// looping through them
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
+
+// using the shortcut of looping through.
+for (let fruit of fruits) {
+    console.log(fruit)
+}
